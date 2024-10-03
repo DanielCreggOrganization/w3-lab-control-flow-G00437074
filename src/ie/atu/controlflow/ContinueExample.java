d61ede30a7db013ff96e236c9a66e1837ab72be3
Adam Devlin <145337473+G00437074@users.noreply.github.com>
2024-10-03 15:22:18 +0000

    uploading exercise 6

--- src/ie/atu/controlflow/ContinueExample.java
+++ src/ie/atu/controlflow/ContinueExample.java
@@ -3,7 +3,7 @@ package ie.atu.controlflow;
 public class ContinueExample {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            if (i == 3) {
+            if (i % 3 == 0) {
                 continue; // Skip this iteration when i is 3
             }
             System.out.println("Iteration: " + i);
