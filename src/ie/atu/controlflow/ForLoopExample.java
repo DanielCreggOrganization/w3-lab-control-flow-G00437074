cc5e627cbd76ce74a11cfcef845aad8884b8cf0d
Adam Devlin <145337473+G00437074@users.noreply.github.com>
2024-10-03 14:56:08 +0000

    uploading exercise 3

---
+++ src/ie/atu/controlflow/ForLoopExample.java
@@ -0,0 +1,9 @@
+package ie.atu.controlflow;
+
+public class ForLoopExample {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 10 ; i+=2) {
+            System.out.println("Iteration: " + i);
+        }
+    }
+}
